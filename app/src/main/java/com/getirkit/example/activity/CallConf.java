4a5fdaad544d2b61c7c9bdafa91768b3f289c458
forest <forester24@outlook.jp>
2017-07-05 16:43:18 +0900

    comit

--- app/src/main/java/com/getirkit/example/activity/CallConf.java
+++ app/src/main/java/com/getirkit/example/activity/CallConf.java
@@ -5,6 +5,55 @@ package com.getirkit.example.activity;
  */
 
 public class CallConf {
+    /**@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.activity_call_conf);
 
+
+        final TextView tvDescription = (TextView) findViewById(R.id.description);
+        final Switch callSwitch = (Switch) findViewById(R.id.callSwitch);
+
+        // スイッチのON・OFF
+        callSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+                if (isChecked) {
+                    // 電話の状況を検知するリスナーをセット
+                    TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                    tm.listen(mPhoneStateListener,
+                            PhoneStateListener.LISTEN_CALL_STATE);
+
+                    tvDescription
+                            .setText(getString(R.string.label_detect_callphone));
+                    tvDescription.setVisibility(View.VISIBLE);
+                } else {
+                    tvDescription.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    /**
+     * 電話の状態を受け取るリスナー.
+     */
+    /**PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
+        public void onCallStateChanged(int state, String number) {
+            if (state == TelephonyManager.CALL_STATE_RINGING) {
+                // 着信を検知
+                Toast.makeText(CallConf.this,
+                        getString(R.string.label_detect_calling, number),
+                        Toast.LENGTH_SHORT).show();
+            } else if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
+                // 通話を検知
+                Toast.makeText(Ch1306.this,
+                        getString(R.string.label_detect_talking, number),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+    */
 }
+
 
