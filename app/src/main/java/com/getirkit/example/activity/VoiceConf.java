a047d481b34cc9368f22b3831c6801e69bfb1680
Kamukun <kawasaki11951104@gmail.com>
2017-07-06 14:37:26 +0900

    音声入力アップグレード

--- app/src/main/java/com/getirkit/example/activity/VoiceConf.java
+++ app/src/main/java/com/getirkit/example/activity/VoiceConf.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.content.ActivityNotFoundException;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.getirkit.example.R;
@@ -25,7 +27,15 @@ public class VoiceConf extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_conf);
 
-        voicestart();
+        // ボタンを設定
+        Button button = (Button)findViewById(R.id.button);
+        // リスナーをボタンに登録
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voicestart();
+            }
+        });
     }
 
     @Override
