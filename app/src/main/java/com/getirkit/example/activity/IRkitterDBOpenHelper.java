ac2148a54d4dafacec4b26180e378911bdb8007d
Kamukun <kawasaki11951104@gmail.com>
2017-07-11 10:59:23 +0900

    dbわかりまてん

--- app/src/main/java/com/getirkit/example/activity/IRkitterDBOpenHelper.java
+++ app/src/main/java/com/getirkit/example/activity/IRkitterDBOpenHelper.java
@@ -11,10 +11,10 @@ import android.util.Log;
 
 public class IRkitterDBOpenHelper extends SQLiteOpenHelper
 {
-    public static final String TAG = "IRkitterDBOpenHelper";
+    private static final String TAG = "IRkitterDBOpenHelper";
 
     static final String DATABASE_NAME = "irkitter.db";
-    static final int DATABASE_VERSION = 1;
+    static final int DATABASE_VERSION = 3;
 
     public IRkitterDBOpenHelper (Context context)
     {
@@ -32,7 +32,7 @@ public class IRkitterDBOpenHelper extends SQLiteOpenHelper
             //infraredテーブルを生成
             database.execSQL("create table infrared("
                     + "redid integer primary key autoincrement,"
-                    + "redpattern text not null)");
+                    + "redpattern text not null);");
 
             //iconテーブルを生成
             database.execSQL("create table icon("
