2c7b3a2fb99adbdc2610cd32a98300c8e3a1428e
健太 <anpantti0720@gmail.com>
2017-07-13 14:06:22 +0900

    7/13

--- app/src/main/java/com/getirkit/example/activity/MainActivity.java
+++ app/src/main/java/com/getirkit/example/activity/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        IRkitDBManager manager = new IRkitDBManager(this);
+        //IRkitDBManager manager = new IRkitDBManager(this);
 
         //Intent intent = new Intent(this, WifiConf.class);
         //startActivity(intent);
