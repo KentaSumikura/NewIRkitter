cb1e91447b1824471c3a7a3f36687871bebdbc75
tanma1210 <1401024@st.asojuku.ac.jp>
2017-07-24 19:36:59 +0900

    変更

--- app/src/main/java/com/getirkit/example/activity/TimerConf.java
+++ app/src/main/java/com/getirkit/example/activity/TimerConf.java
@@ -1,5 +1,6 @@
 package com.getirkit.example.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,148 +8,147 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.sql.Time;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.widget.Toast;
 
 import com.getirkit.example.R;
 
 public class TimerConf extends AppCompatActivity {
-    TextView textView;
-    TextView textView1;
-    TextView textView2;
-    TextView conbr;
-    TextView anthr;
-    TextView date;
-    EditText editTexthour;//時間入力させるとこ
-    EditText editTextminute;//分入力させるとこ
-    Button set;
-    int sethour;
-    int setminute;
-    int nowhour;
-    int nowminute;
-    Thread thread = null;
+    private static int bid2 = 2;
+    private static int bid1 = 1;
 
+    private Button button2, button3;
+    private TextView textView;
+    public EditText editTextmonth;
+    public EditText editTextdate;
+    public EditText editTexthour;//時間入力させるとこ
+    public EditText editTextminute;//分入力させるとこ
+    private int year, month, date, hour, minute, second, msecond;
+    private int setText,setText2,setText3,setText4;
 
+    private int sethour;
+    private int setminute;
+    private int setmonth;
+    private int setdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer_conf);
 
-        thread = new Thread(thread);
-        thread.start();
-
-        Log.d("ああ", "onCreate()");
-
-        textView = (TextView) findViewById(R.id.hour_text);
-        textView1 = (TextView)findViewById(R.id.minute_text);
-
-        editTexthour = (EditText) findViewById(R.id.hour);
-        editTextminute = (EditText) findViewById(R.id.minute);
-        Log.d("やまがみ","やまもと");
-        set = (Button) findViewById(R.id.set);
-        set.setOnClickListener(new View.OnClickListener() {
+        //アラームの取り消し
+        button2 = (Button) this.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                // 5秒後に設定
+                calendar.add(Calendar.SECOND, 5);
 
-                //バックグラウンド処理を記載
-                //別クラスつくってIntent でなげる
-                //start とstopつくる
-                //break
+                Intent intent = new Intent(getApplicationContext(), AlarmBroadcastReceiver.class);
+                intent.putExtra("intentId", 1);
+                // PendingIntentが同じ物の場合は上書きされてしまうので requestCode で区別する
+                PendingIntent pending = PendingIntent.getBroadcast(getApplicationContext(), bid1, intent, 0);
 
+                // アラームをセットする
+                AlarmManager am = (AlarmManager) TimerConf.this.getSystemService(ALARM_SERVICE);
+                // 約10秒で 繰り返し
+                am.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 10000, pending);
+
+                // トーストで設定されたことをを表示
+                Toast.makeText(getApplicationContext(), "ALARM 1", Toast.LENGTH_SHORT).show();
+
+                // 無理やりですが、アプリを一旦終了します。この方法はバックグラウンドに移行させるための方便で推奨ではありません
+                close();
+            }
+        });
+
+        textView = (TextView) findViewById(R.id.text_view);
+        // 協定世界時 (UTC)です適宜設定してください
+        editTextmonth = (EditText) findViewById(R.id.month);
+        editTextdate = (EditText) findViewById(R.id.date);
+        editTexthour = (EditText) findViewById(R.id.hour);
+        editTextminute = (EditText) findViewById(R.id.minute);
+
+
+        // 日時を指定したアラーム
+        button3 = (Button) this.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("やまがみ","やまもと");
 
                 // エディットテキストの時間を取得
-                String setText = editTexthour.getText().toString();
+                setText = Integer.parseInt(editTexthour.getText().toString());
                 // 取得したテキストを hour text に張り付ける (時間)
-                textView.setText(setText);
+                textView.setText(String.valueOf(setText));
                 // エディットテキストの分を取得
-                String setText2 = editTextminute.getText().toString();
+                setText2 = Integer.parseInt(editTextminute.getText().toString());
                 // 取得したテキストを minuteText に張り付ける(分)
-                textView.setText(setText2);
+                textView.setText(String.valueOf(setText2));
+                // エディットテキストの日付を取得
+                setText3 = Integer.parseInt(editTextdate.getText().toString());
+                // 取得したテキストを date text に張り付ける (時間)
+                textView.setText(String.valueOf(setText3));
+                // エディットテキストの時間を取得
+                setText4 = Integer.parseInt(editTextmonth.getText().toString());
+                // 取得したテキストを hour text に張り付ける (時間)
+                textView.setText(String.valueOf(setText4));
 
+                /*
                 sethour = new Integer(setText);//nowhourに入力させた時間をいれる
-
                 setminute = new Integer(setText2);//nowhourに入力させた分をいれる
+                setdate= new Integer(setText3);
+                setmonth = new Integer(setText4);
+                */
 
-                Log.d("test",String.valueOf(sethour));
 
-                Log.d("やまもと",String.valueOf(setminute));
+                sethour = setText;//nowhourに入力させた時間をいれる
+                setminute = setText2;//nowhourに入力させた分をいれる
+                setdate= setText3;
+                setmonth = setText4;
 
-                while (true) {
+                year = 2017;
+                month = setmonth - 1;
+                date =setdate;
+                hour = sethour;
+                minute = setminute;
+                second = 0;
+                msecond = 0;
 
-                    try {
-                        Thread.sleep(10000);
-                    }catch (InterruptedException e){
-                    }
+                Calendar calendar2 = Calendar.getInstance();
+                // 過去の時間は即実行されます
+                calendar2.set(Calendar.YEAR, year);
+                calendar2.set(Calendar.MONTH, month);
+                calendar2.set(Calendar.DATE, date);
+                calendar2.set(Calendar.HOUR_OF_DAY, hour);
+                calendar2.set(Calendar.MINUTE, minute );
+                calendar2.set(Calendar.SECOND, second);
+                calendar2.set(Calendar.MILLISECOND, msecond);
 
-                    Log.d("test","のだ");
-                    // 現在日時の取得
-                    Time now = new Time(System.currentTimeMillis());
-                    // 日時のフォーマットオブジェクト作成
-                    final DateFormat formatter = new SimpleDateFormat("hh");
-                    // now変数にいれた現在日時をフォーマット
-                    final String nowText = formatter.format(now);
-                    // 日時のフォーマットオブジェクト作成
-                    final DateFormat formatter2 = new SimpleDateFormat("mm");
-                    // now変数に入れた分をフォーマット
-                    final String nowText2 = formatter2.format(now);
-                    // 表示
-                    TextView date = (TextView) findViewById(R.id.date);
-                    date.setText(nowText + ":" + nowText2);
+                Intent intent = new Intent(getApplicationContext(), AlarmBroadcastReceiver.class);
+                intent.putExtra("intentId", 2);
+                PendingIntent pending = PendingIntent.getBroadcast(getApplicationContext(), bid2, intent, 0);
 
-                    nowhour = new Integer(nowText);
-                    nowminute = new Integer(nowText2);
+                // アラームをセットする
+                AlarmManager am = (AlarmManager) TimerConf.this.getSystemService(ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, calendar2.getTimeInMillis(), pending);
+                Toast.makeText(getApplicationContext(), "ALARM 2", Toast.LENGTH_SHORT).show();
 
-                    Log.d("test",String.valueOf(nowhour));
-
-                    Log.d("test111",String.valueOf(nowminute));
-
-            /*
-            if (sethour > 12) {
-                sethour = sethour - 12;//24時間表記を12時間表に変更
-                Log.d("ave","mario");
-            }
-            */
-
-                    if (sethour == nowhour) {//設定時と現在時の比較
-
-                        conbr = (TextView) findViewById(R.id.conbr);
-
-                        Log.d("amaging","break");
-
-                        conbr.setText("成功");
-
-                        conbr.setText("時間おｋ");
-
-                        if (setminute == nowminute) {//設定分と現在分の比較
-
-                            //表示（実際は赤外線送信処理）
-                            Log.d("おｋ","");
-
-                            anthr = (TextView) findViewById(R.id.anthr);
-
-                            anthr.setText("成功");
-                            break;
-                        } else {
-
-                            Log.d("だめ","");
-
-                            anthr = (TextView) findViewById(R.id.anthr);
-
-                            anthr.setText("失敗");
-                        }
-                    }
-                    Log.d("つのだ","");
-                    //break;
-                }
+                String setTime = "設定時間(UTC)："+year+"/"+(month + 1)+"/"+date+" "+hour+":"+minute+":"+second+"."+msecond;
+                textView.setText(setTime);
 
             }
-        });//?
-
-
+        });
     }
 
-
-
+    private void close() {
+        finish();
     }
+}
+
+
+
 
